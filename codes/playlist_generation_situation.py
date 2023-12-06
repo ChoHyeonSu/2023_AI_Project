757b862e6288f979e2eac9a1a49cf968868fc2c9
tkfhdk123 <tkfhdk123@gmail.com>
2023-12-06 13:50:21 +0900

    step2 code update

--- codes/playlist_generation_situation.py
+++ codes/playlist_generation_situation.py
@@ -13,55 +13,15 @@ def random_sampling(df, count=10):
     print(f'Index: {random_list}')
     print(f'Song Name: {random_name}')
 
-def label_sampling(df, index, count=10):
-    # 샘플곡의 확률 분포로 모든 곡들에 대한 kl divergence 생성
-    kl_divergence_total_list = []
-    kl_divergence_label_list = []
-    sample_prob_str = df.loc[index, 'probability']
-
-    for i in range(len(df)):
-        if i == index:
-            kl_divergence_total_list.append('Nan')
-            continue
-        new_prob_str = df.loc[i, 'probability']
-
-        sample_prob = str_to_list(sample_prob_str)
-        new_prob = str_to_list(new_prob_str)
-        
-        kld = kl_divergence_with_smoothing(sample_prob, new_prob)
-        kl_divergence_total_list.append(kld)
-
-    # 샘플곡의 라벨과 동일한 곡 N개 추출
-    sample_label = df.loc[index, 'Emotion']
-    new_df = df[df['Emotion'] == sample_label].sample(count)
-
-    label_list = new_df.index.values.tolist()
-    label_name = new_df['song_name'].tolist()
-
-    # 같은 라벨 곡들의 kl divergence를 보기 위한 코드
-    for i in label_list:
-        kl_divergence_label_list.append(kl_divergence_total_list[i])
-
-    print('-'*30)
-    print(f'Index: {label_list}')
-    print(f'Song Name: {label_name}')
-    print(f'KL Divergence: {kl_divergence_label_list}')
-
-def baseline_sampling(df, df2, index, count=10):
+def baseline_sampling(df, df2, prob_list, count=10):
     # 샘플곡의 확률 분포로 모든 곡들에 대한 kl divergence 생성
     kl_divergence_total_list = []
     kl_divergence_base_list = []
-    sample_prob_str = df.loc[index, 'probability']
+    sample_prob = prob_list
 
     for i in range(len(df2)):
-        if i == index:
-            kl_divergence_total_list.append('Nan')
-            continue
         new_prob_str = df2.loc[i, 'probability']
-
-        sample_prob = str_to_list(sample_prob_str)
         new_prob = list(map(float, new_prob_str[1:-1].split()))
-        
         kld = kl_divergence_with_smoothing(sample_prob, new_prob)
         kl_divergence_total_list.append(kld)
     
@@ -78,21 +38,15 @@ def baseline_sampling(df, df2, index, count=10):
     print(f'Baseline Model Song Name: {base_name}')
     print(f'KL Divergence: {kl_divergence_base_list}')
 
-def our_sampling(df, df2, index, count=10):
+def our_sampling(df, df2, prob_list, count=10):
     # 샘플곡의 확률 분포로 모든 곡들에 대한 kl divergence 생성
     kl_divergence_total_list = []
     kl_divergence_our_list = []
-    sample_prob_str = df.loc[index, 'probability']
+    sample_prob = prob_list
 
     for i in range(len(df2)):
-        if i == index:
-            kl_divergence_total_list.append('Nan')
-            continue
         new_prob_str = df2.loc[i, 'probability']
-
-        sample_prob = str_to_list(sample_prob_str)
-        new_prob = str_to_list(new_prob_str)
-        
+        new_prob = str_to_list(new_prob_str)     
         kld = kl_divergence_with_smoothing(sample_prob, new_prob)
         kl_divergence_total_list.append(kld)
     
@@ -118,14 +72,14 @@ song_prob_gold = pd.read_csv(song_prob_gold_path)
 song_prob_baseline = pd.read_csv(song_prob_baseline_path)
 song_prob_our = pd.read_csv(song_prob_our_path)
 
-# 노래 한 곡을 샘플링하는 코드
-sample = random.randint(0, len(song_prob_gold))
-sample_label = song_prob_gold.loc[sample, 'Emotion']
-sample_name = song_prob_gold.loc[sample, 'song_name']
+# 상황 및 감정 분포 리스트
+situation_list = ['시험 준비할 때', '좋아하는 사람과 첫 데이트할 때', '새로운 도시로 이사할 때', '친한 친구의 결혼식에 참석할 때', '이별을 겪고 있을 때', '전 여자친구가 생각날 때', '중요한 소식을 기다리고 있을 때', '전 여자친구를 길에서 만났을 때', '연말에 듣는 노래', '일할 때 듣는 노래']
+ratio_list = [[5, 0, 10, 75, 5, 5], [10, 30, 50, 5, 0, 5], [10, 0, 20, 30, 30, 10], [0, 70, 20, 0, 0, 10], [30, 0, 0, 10, 30, 30], [30, 0, 0, 0, 20, 50], [10, 0, 40, 40, 5, 5], [20, 10, 15, 30, 15, 10], [5, 60, 30, 5, 0, 0], [0, 40, 35, 15, 0, 10]]
+prob_list = (np.array(ratio_list) / 100)
 
-print(f'Sample Emotion: {sample_label}')
-print(f'Sample Name: {sample_name}')
+# 상황 하나를 고르는 과정
+sample_index = int(input('0 ~ 9까지 중에 입력하시오\n'))
+
 random_sampling(song_prob_gold)
-label_sampling(song_prob_gold, sample)
-baseline_sampling(song_prob_gold, song_prob_baseline, sample)
-our_sampling(song_prob_gold, song_prob_our, sample)
+baseline_sampling(song_prob_gold, song_prob_baseline, prob_list[sample_index])
+our_sampling(song_prob_gold, song_prob_our, prob_list[sample_index])
